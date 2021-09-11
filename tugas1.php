064ccc9516671a35659fb8190bfee270cea283c8
MuhamadRizaldiApriansah <87842100+MuhamadRizaldiApriansah@users.noreply.github.com>
2021-09-11 12:58:04 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rizaldi page</title>
+</head>
+<body>
+<style>
+* {
+    padding:0px;
+    margin:0px;
+}
+.container {
+    width:80%;
+    margin:auto;
+}
+.head {
+    height:40px;
+    background:#F3FFE3;
+    text-align: right;
+    padding-right:10px;
+    padding-top: 10px;
+}
+.footer {
+    height:40px;
+    background:#F3FFE3;
+    text-align:right;
+    padding-right:10px;
+    padding-top:10px;
+}
+.head a {
+    color: darkslategray;
+    text-decoration-line:none;
+}
+.head a:hover {
+    color:gray;
+    text-decoration-line:none;
+}
+.head h1{
+
+}
+.ban {
+    height:300px;
+    background:#5F9EA0;
+}
+.footer {
+    height:80px;
+    background:#F3FFE3;
+    text-align:right;
+    line-height:80px;
+    /*display:flex;
+    align-items:center;
+    justify-content:end !important;*/
+}
+.footer a {
+    color:darkslategray;
+    text-decoration:none;
+}
+.footer a:hover {
+    color:gray;
+    text-decoration:none;
+}
+.content {
+    height:600px;
+}
+.sidebar {
+    height:100%;
+    width:25%;
+    list-style:none;
+    float:left;
+    background-color:beige;
+}
+.sidebar li a {
+    display:block;
+    padding:25px;
+    text-decoration:none;
+    color:cadetblue;
+    font-weight:bold;
+    border-bottom:2px beige;
+    transition:background-color 0.5s;
+}
+.sidebar li a: hover {
+    background-color:grey;
+    color:black;
+}
+</style>
+
+<div class="container">
+    <div class="head">
+        <a href="">Home</a>&nbsp;|
+        <a href="">Profile</a>&nbsp;|
+        <a href="">Contact</a>
+</div>
+
+<div class="ban">
+    <center><img style="width:50%;height:50%;padding:20px" src="https://lh3.googleusercontent.com/proxy/3xGH7rM_X3D6zpGRgbPaQH2hmbZJ7NLNcNvwQnnN8INjpfv_zDY9UCj5XwL3K143Pn4biQ-QZcHYy9Ni_TPu321TjcpZXbzP8M7iVCx2_dhbT9I">
+</div>
+
+<div class="content">
+    <ul class="sidebar">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">User</a></li>
+        <li><a href="#">Setting</a></li>
+        <li><a href="#">Help</a></li>
+    </ul>
+
+    <div style="width:75%;height:100%;background:#5F9EA0;float:right;"
+    <center><h1 style="color:beige;padding:10px;">Buku Adalah Jendela Dunia</h1></center><br>
+    <p style="color:beige;padding:10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa aut sit, enim nisi unde consequatur, aspernatur
+        officia at placeat minima quis itaque optio natus aliquam facilis harum maxime molestias voluptatum fuga. Ipsum
+        obcaecati voluptate deserunt architecto, necessitatibus sequi, error aut excepturi ullam expedita voluptas iste?
+        Magnam voluptas nihil facilis nemo est placeat quas numquam optio ratione, iusto voluptates magni sunt,
+        doloribus cupiditate accusamus, temporibus modi facere debitis. Nam, vel error! Debitis nulla porro eius tempore
+        id sint quis similique impedit hic, tempora laboriosam velit quo. Ratione architecto sed sunt eius reprehenderit
+        necessitatibus doloribus, quos quia incidunt aspernatur. Repudiandae aliquid eveniet mollitia repellendus
+        deserunt quo quisquam. Ipsam culpa magni nulla mollitia, iste aliquam voluptate eaque in doloribus autem dolorum
+        quaerat alias quisquam! Vitae doloremque corrupti eius, laboriosam inventore dignissimos est laborum qui fugit
+        totam quisquam non perspiciatis beatae veritatis voluptatibus ducimus debitis.</p>
+    <p style="color:beige;padding:10px;">Adipisci iste consectetur enim aspernatur magni ad similique possimus veniam vero aperiam illo velit, dolor suscipit aut vitae sapiente
+        praesentium, quia dolores officia dolorem? Modi quidem totam, consequatur voluptatibus dolore tempora similique
+        ullam, illum et nesciunt magni maxime quod inventore ab officiis mollitia perferendis voluptate dolor voluptatem
+        aut dignissimos? Incidunt sed nulla non dicta eaque, voluptates qui assumenda optio error blanditiis
+        perspiciatis temporibus, esse a quibusdam maiores sint inventore molestiae nobis nemo! Officia eum quibusdam,
+        perspiciatis in iusto, aliquid accusamus qui maxime iste, minima maiores! Commodi quae porro voluptas!
+        Aspernatur deserunt est molestiae eveniet repellendus, nisi voluptatibus maiores! Voluptatibus tenetur
+        consectetur voluptatum molestias error provident eum quas ex consequatur minus iste nam deleniti consequuntur,
+        soluta commodi sint incidunt doloremque architecto nulla repellendus necessitatibus temporibus sunt deserunt.
+        Molestiae dolor totam aliquid perspiciatis adipisci nostrum eveniet dolore, tempora omnis exercitationem
+        eligendi laboriosam nesciunt a architecto error distinctio cum, ipsam ducimus voluptatibus sed neque. Veniam
+        iure dolor corporis aperiam quod commodi nam.</p>
+</div>
+    </div>
+
+    <div class="footer">
+        <ul>
+            <a href="">Home</a>&nbsp;|
+            <a href="">Profile</a>&nbsp;|
+            <a href="">Contact</a>
+        </ul>
+</div>
+
+</div>
+
+</body>
+</html>
+</body>
+</html>
